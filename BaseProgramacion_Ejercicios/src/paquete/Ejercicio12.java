aaf90297f6c362392d1556b98943fecd432f2f83
alex <alex.getafe11@gmail.com>
2021-09-22 17:30:24 +0200

    Ejercicio terminado

--- BaseProgramacion_Ejercicios/src/paquete/Ejercicio12.java
+++ BaseProgramacion_Ejercicios/src/paquete/Ejercicio12.java
@@ -7,33 +7,29 @@ public class Ejercicio12 {
 	public static void main(String[] args) {
 
 		/*Programa que muestra los números primos existentes entre el 1 y el 100. NOTA: Un
-		número es primo si tiene dos únicos divisores que son el 1 y él mismo*/
+		número es primo si tiene dos únicos divisores que son el 1 y él mismo*/		
 		
-		
-		int numero;
 		int resto;
-		Scanner sc= new Scanner(System.in);
-		boolean primo = true;
+		boolean primo ;
 		
-		System.out.println("Introduce numero");
-		numero=sc.nextInt();
+		for(int numero=2;numero<100;numero++) {
+		primo=true;
 		
-		if (numero<2) {
-			primo = false;
-		}
-		for(int i=2; i<numero && primo==true ;i++) {
+			if(numero<2) {
+				primo=false;
+			}
+			for(int i=2; i<numero && primo==true ;i++) {
 			
 			resto= numero%i;
 			
 			if(resto == 0) primo = false;		
-		}
+			}
 		
-	
-		String mensaje= (primo) ? "Es primo" : "No es primo";
-		
+		String mensaje= "El numero " + numero + " es primo";
+		if(primo)
 		System.out.println(mensaje);
-		
-		sc.close();
+		}		
+
 		
 		}
 	}
