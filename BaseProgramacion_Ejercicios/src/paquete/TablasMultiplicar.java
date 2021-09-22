0a4c638ea914a34620f2657b44a1f2543926d31f
alex <alex.getafe11@gmail.com>
2021-09-22 18:32:02 +0200

    Terminado + suma

--- BaseProgramacion_Ejercicios/src/paquete/TablasMultiplicar.java
+++ BaseProgramacion_Ejercicios/src/paquete/TablasMultiplicar.java
@@ -10,15 +10,16 @@ public class TablasMultiplicar {
 		
 		
 		int resultado;
-		
+		int suma=0;
 		for(int numero=1; numero<=10; numero++)
 		{	
 			for(int multiplicado=1;multiplicado<=10;multiplicado++) 
 			{
 				resultado= numero * multiplicado;
+				suma += resultado;
 				System.out.println(numero + " X " + multiplicado + " = " + resultado);
 				if(multiplicado==10) {
-					System.out.println();
+					System.out.println("La suma de todo es : " + suma + "\n" );
 				}
 			}
 			
