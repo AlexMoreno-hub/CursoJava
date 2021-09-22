fedbb5daec62576f8d3a39affca0b69c6b3fcf1f
alex <alex.getafe11@gmail.com>
2021-09-22 19:16:27 +0200

    Ejercicio terminado

--- Arrays/src/Ejercicio19.java
+++ Arrays/src/Ejercicio19.java
@@ -1,9 +1,38 @@
+import java.util.Scanner;
 
 public class Ejercicio19 {
 
 	public static void main(String[] args) {
 
-		//
+		/*19. Realiza un programa que lea desde teclado 10 números y posteriormente muestre
+		solamente los que sean pares.*/
+		
+		int n = 10;
+		//array de numeros ; le
+		int numeros[]=new int[n];
+		
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Intro. " + n + " numeros: ");
+		
+		//rellenamos el array
+		for(int i=0; i<n;  i++)
+		{
+			numeros[i] = sc.nextInt();  
+		}
+		sc.close();
+		
+		//recorremos el array
+		System.out.println("Los numeros pares son:");
+		for(int i=0; i<n; i++)
+		{
+			if(numeros[i]%2==0)
+			{
+				System.out.println(numeros[i]);				
+			}
+		}
+		
+		
 	}
 
 }
