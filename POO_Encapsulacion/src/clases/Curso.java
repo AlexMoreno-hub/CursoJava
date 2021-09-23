08d4982e9932284bf4453b3b76f419165914948c
alex <alex.getafe11@gmail.com>
2021-09-23 17:28:06 +0200

    Puebas de Encapsulacion

---
+++ POO_Encapsulacion/src/clases/Curso.java
@@ -0,0 +1,33 @@
+package clases;
+
+public class Curso {
+	
+	//Campos
+	
+	private String denominacion;
+	private int horas;
+	private String academia;
+
+	
+	public String getDenominacion() {
+		return denominacion;
+	}
+	public void setDenominacion(String denominacion) {
+		this.denominacion = denominacion;
+	}
+	public int getHoras() {
+		return horas;
+	}
+	public void setHoras(int horas) {
+		this.horas = horas;
+	}
+	public String getAcademia() {
+		return academia;
+	}
+	public void setAcademia(String academia) {
+		this.academia = academia;
+	}
+	
+
+	
+}
---
+++ POO_Encapsulacion/src/ejecuta/Inicio.java
@@ -0,0 +1,17 @@
+package ejecuta;
+
+import clases.Curso;
+
+public class Inicio {
+
+	public static void main(String[] args) {
+
+		Curso java= new Curso();
+		
+		String denominacion;
+		
+		denominacion = java.getDenominacion();
+		
+	}
+
+}
